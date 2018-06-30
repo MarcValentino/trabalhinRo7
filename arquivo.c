6dfc97067752a8c022cdf5a4f0c0cedf94c754df
MarcValentino <mvalentino@id.uff.br>
2018-06-30 18:30:42 -0300

    Leitura de arquivo e struct de infos

--- arquivo.c
+++ arquivo.c
@@ -2,20 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct info{
+  char cantor[100];
+  int ano;
+  int nMusicas;
+  int minutos;
+  char nmAlbum[100];
+}Info;
+
 void leLinhas(char *nome){
   FILE *arq;
   arq = fopen("bd", "r");
   char linha[256], *token;
   while (fgets(linha, sizeof(linha), arq)){
+        Info *infos = (Info *) malloc(sizeof(Info));
         token = strtok(linha, "/");
         //while(token){
         //  printf("%s\n", token);
         //  token = strtok(NULL, "/");
         //}
-        printf("%s\n", token);
+        strcpy(infos->cantor, token);
         token = strtok(NULL, "/");
-        int ano = atoi(token);
-        printf("%d\n", ano);
+        char chave[200];
+        strcpy(chave, infos->cantor);
+        strcat(chave, token);
+        //printf("%s\n", chave);
+        infos->ano = atoi(token);
+        token = strtok(NULL, "/");
+        infos->nMusicas = atoi(token);
+        token = strtok(NULL, "/");
+        infos->minutos = atoi(token);
+        token = strtok(NULL, "/");
+        strcpy(infos->nmAlbum, token);
+        //Insere(chave, infos, arv);
       }
   }
 
